3ab37fa0d817cf93043c13396e8b31e9a75a2f87
Abdullah <hasan.techtomorrow@gmail.com>
2023-06-05 23:27:51 +0600

    coaches list

--- src/components/pages/coaches/CoachesList.vue
+++ src/components/pages/coaches/CoachesList.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-        Content goes here
-      </div>
+    <section>Filter</section>
+
+    <section>
+
+        <div class="controls">
+<button>Refresh</button>
+<router-link to="/register">Register</router-link>
+        </div>
+        <ul v-if="isCoach">
+            <li v-for="coach in filteredCoaches" :key="coach.id">
+{{ coach.firstName }}
+            </li>
+        </ul>
+        <h3 v-else>No coaches found</h3>
+    </section>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
 *{
     box-sizing: border-box;
 }
@@ -18,3 +32,18 @@ body{
     margin: 0;
 }
 </style>
+
+
+<script>
+export default {
+    computed:{
+      filteredCoaches(){
+        return this.$store.getters['coaches/coaches'];
+      },
+
+      isCoach(){
+        return this.$store.getters['coaches/hasCoaches'];
+      }
+    }
+}
+</script>
